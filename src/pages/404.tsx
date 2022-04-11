ed056ef54d07205b76215c486237e69b5787dd96
Daniel Huang <99danielh@gmail.com>
2022-04-11 11:12:00 -0400

    fix: add 404 margin

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Custom404() {
 	return (
 		<>
-			<h1 className="heading text-amber-400">
+			<h1 className="mb-2 heading text-amber-400">
 				404 &ndash; page not found...
 			</h1>
-			<p className="mb-4">
+			<p>
 				Seems like this page doesn&apos;t exist. Let&apos;s{" "}
 				<Link href="/">
 					<a className="border-b-2 border-b-white hover:border-b-transparent transition">
