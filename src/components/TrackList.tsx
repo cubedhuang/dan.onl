cabf899b300bcf4e8a1d63da36f47adb689c2c81
cubedhuang <99danielh@gmail.com>
2022-04-16 22:39:27 -0400

    fix: mask-image > background for mobile

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -7,7 +7,7 @@ interface TrackListProps {
 
 export function TrackList({ tracks, priority = false }: TrackListProps) {
 	return (
-		<div className="relative w-screen ml-[calc(-50vw)] left-1/2 mb-12 before:fixed before:inset-0 before:bg-[linear-gradient(to_right,#0000_0%,#0000_80%,#000_100%)] lg:before:bg-[linear-gradient(to_right,#000_0%,#0000_25%,#0000_75%,#000_100%)] before:z-10">
+		<div className="relative w-screen ml-[calc(-50vw)] left-1/2 mb-12 before:fixed before:inset-0 [mask-image:linear-gradient(to_right,#000_0%,#000_75%,#0000_100%)] lg:[mask-image:linear-gradient(to_right,#0000_0%,#000_25%,#000_75%,#0000_100%)] before:z-10">
 			<div className="lg:px-[calc(50vw-400px)] md:px-[calc(50vw-350px)] px-8 grid grid-rows-2 grid-cols-[repeat(12,calc(9rem))] md:grid-cols-[repeat(12,calc(12rem))] lg:grid-cols-[repeat(12,calc(14rem))] grid-flow-col gap-4 overflow-scroll no-scrollbar">
 				{tracks.map((track, i) => (
 					<a
