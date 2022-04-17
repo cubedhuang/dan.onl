983726c5dbd3cf84b17f5625a9c4b347fd79d569
cubedhuang <99danielh@gmail.com>
2022-04-16 23:32:43 -0400

    feat(TrackList): add scroll snap

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -7,13 +7,13 @@ interface TrackListProps {
 
 export function TrackList({ tracks, priority = false }: TrackListProps) {
 	return (
-		<div className="relative w-screen ml-[calc(-50vw)] left-1/2 mb-12 before:fixed before:inset-0 [mask-image:linear-gradient(to_right,#000_0%,#000_75%,#0000_100%)] lg:[mask-image:linear-gradient(to_right,#0000_0%,#000_25%,#000_75%,#0000_100%)] before:z-10">
-			<div className="lg:px-[calc(50vw-400px)] md:px-[calc(50vw-350px)] px-8 grid grid-rows-2 grid-cols-[repeat(12,calc(9rem))] md:grid-cols-[repeat(12,calc(12rem))] lg:grid-cols-[repeat(12,calc(14rem))] grid-flow-col gap-4 overflow-scroll no-scrollbar">
+		<div className="relative w-screen ml-[calc(-50vw)] left-1/2 mb-12 [mask-image:linear-gradient(to_right,#000_0%,#000_75%,#0000_100%)] lg:[mask-image:linear-gradient(to_right,#0000_0%,#000_25%,#000_75%,#0000_100%)] before:z-10">
+			<div className="lg:px-[calc(50vw-400px)] md:px-[calc(50vw-350px)] px-8 lg:scroll-pl-[calc(50vw-400px)] md:scroll-pl-[calc(50vw-350px)] scroll-pl-8 grid grid-rows-2 grid-cols-[repeat(12,calc(9rem))] md:grid-cols-[repeat(12,calc(12rem))] lg:grid-cols-[repeat(12,calc(14rem))] grid-flow-col gap-4 overflow-scroll no-scrollbar snap-x snap-mandatory">
 				{tracks.map((track, i) => (
 					<a
 						href={track.external_urls.spotify}
 						key={track.id}
-						className="group relative rounded-lg before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-0 before:transition before:duration-300 hover:before:opacity-50"
+						className="group relative snap-start snap-always rounded-lg before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-0 before:transition before:duration-300 hover:before:opacity-50"
 					>
 						<div className="w-full rounded-lg overflow-hidden">
 							<Image
