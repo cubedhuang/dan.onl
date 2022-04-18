c78ff3963fee53c8f41d83333738de8d84e03cf6
cubedhuang <99danielh@gmail.com>
2022-04-17 20:17:07 -0400

    feat: add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true"
+});
+
+module.exports = withBundleAnalyzer({
 	reactStrictMode: true,
 	swcMinify: true,
 	i18n: { locales: ["en-US"], defaultLocale: "en-US" },
 	images: {
 		domains: ["cdn.discordapp.com", "i.scdn.co"]
 	}
-};
+});
