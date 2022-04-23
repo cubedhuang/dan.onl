38a70aeea5d98e13ef27a4406c247b0795c60655
cubedhuang <99danielh@gmail.com>
2022-04-22 21:00:33 -0400

    refactor(projects): preserve parameter names

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -13,14 +13,14 @@ export default function Projects() {
 
 			<h1 className="heading mb-8">Projects 📦</h1>
 
-			{projects.map(({ name, description, image: bg, url: href }) => (
+			{projects.map(({ name, description, image, url }) => (
 				<div
 					key={name}
 					className="mb-4 flex flex-col rounded-lg bg-slate-900"
 				>
 					<div className="relative h-24">
 						<Image
-							src={bg}
+							src={image}
 							alt=""
 							layout="fill"
 							objectFit="cover"
@@ -37,7 +37,7 @@ export default function Projects() {
 								<p className="text-lg">{description}</p>
 							</div>
 							<a
-								href={href}
+								href={url}
 								target="_blank"
 								rel="noreferrer noopener"
 								className="ml-auto transition hover:opacity-80"
