5c64f0938c11fb68266fe93afb873c9521aa51fd
cubedhuang <99danielh@gmail.com>
2022-05-14 17:41:43 -0400

    feat(anime): add background for image loading

--- src/pages/anime.tsx
+++ src/pages/anime.tsx
@@ -19,13 +19,13 @@ export default function Anime() {
 
 			<p className="text-lg mb-4">Some anime I&apos;ve watched.</p>
 
-			{anime.map(({ name, description, image, url }) => (
+			{anime.map(({ name, description, image, url }, i) => (
 				<a
 					key={name}
 					href={url}
 					target="_blank"
 					rel="noreferrer noopener"
-					className="group relative flex items-center justify-items-start h-40 px-4 sm:px-8 mb-4 overflow-hidden rounded-lg before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-0 before:transition before:duration-300 hover:before:opacity-50"
+					className="group relative flex items-center justify-items-start h-40 px-4 sm:px-8 mb-4 overflow-hidden bg-slate-900 rounded-lg before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-0 before:transition before:duration-300 hover:before:opacity-50"
 				>
 					<Image
 						src={image}
@@ -33,6 +33,7 @@ export default function Anime() {
 						layout="fill"
 						objectFit="cover"
 						className="rounded-lg transition duration-300 group-hover:scale-[1.02]"
+						priority={i < 3}
 					/>
 					<div className="z-20 w-full transition duration-300 scale-95 opacity-0 group-hover:scale-100 group-hover:opacity-100">
 						<h2 className="font-bold text-3xl">{name}</h2>
