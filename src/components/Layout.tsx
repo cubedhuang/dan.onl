59922d425ca56e2ddfb5321c4ec12a7710a2c58f
cubedhuang <99danielh@gmail.com>
2022-07-08 15:19:55 -0400

    refactor: make navbar self closing in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
 	return (
 		<div className="md:container md:px-0 mx-auto my-24 px-8 text-white text-xl">
-			<Navbar></Navbar>
+			<Navbar />
 			<main className="main">{children}</main>
 		</div>
 	);
