6fb8254dcc234eb811ce6c6c1eda0875da0e0c56
cubedhuang <99danielh@gmail.com>
2022-07-08 22:05:11 -0400

    feat: add spotify progress bar

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -1,10 +1,21 @@
+import { PauseIcon, PlayIcon } from "@heroicons/react/solid";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 import type {
 	NowPlayingResponseError,
 	NowPlayingResponseSuccess
 } from "../pages/api/nowPlaying";
+
+const formatDuration = (ms: number) => {
+	const seconds = Math.floor((ms / 1000) % 60)
+		.toString()
+		.padStart(2, "0");
+	const minutes = Math.floor(ms / 1000 / 60);
+
+	return `${minutes}:${seconds}`;
+};
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
@@ -18,6 +29,27 @@ export default function Spotify() {
 	// and I don't wanna deal with that.
 	const track = data?.item && "album" in data.item ? data.item : null;
 
+	const [time, setTime] = useState(data?.progress_ms);
+
+	useEffect(() => {
+		if (!data?.progress_ms || !track) return;
+
+		const started = Date.now();
+
+		const interval = setInterval(() => {
+			setTime(
+				data.is_playing
+					? Math.min(
+							data.progress_ms! + Date.now() - started,
+							track.duration_ms
+					  )
+					: data.progress_ms
+			);
+		}, 100);
+
+		return () => clearInterval(interval);
+	}, [data]);
+
 	return (
 		<div className="flex gap-2 items-center text-base leading-snug">
 			<div className="w-16 h-16 md:w-20 md:h-20 flex-shrink-0">
@@ -30,7 +62,7 @@ export default function Spotify() {
 					className="rounded-lg"
 				/>
 			</div>
-			<div>
+			<div className="basis-full">
 				<p>
 					{track ? (
 						<>
@@ -77,17 +109,48 @@ export default function Spotify() {
 					) : null}
 				</p>
 				<p className="opacity-80 flex items-center gap-1">
-					<span className="w-4 h-4">
-						<Image
-							src="/images/spotify.png"
-							alt=""
-							width={48}
-							height={48}
-							className="w-4 h-4"
-						/>
-					</span>
-					{track ? "Listening on " : null}
-					Spotify
+					{track ? (
+						<span className="block w-full max-w-sm mt-2">
+							<span className="block h-0.5 rounded bg-slate-900">
+								<span
+									className="block h-full bg-slate-400"
+									style={{
+										width: `${
+											(time! / track.duration_ms) * 100
+										}%`
+									}}
+								/>
+							</span>
+							<span className="flex items-center text-sm">
+								<span className="basis-full">
+									{formatDuration(time!)}
+								</span>
+								<span>
+									{data?.is_playing ? (
+										<PauseIcon className="text-white h-4 w-4" />
+									) : (
+										<PlayIcon className="text-white h-4 w-4" />
+									)}
+								</span>
+								<span className="basis-full text-right">
+									{formatDuration(track.duration_ms)}
+								</span>
+							</span>
+						</span>
+					) : (
+						<>
+							<span className="w-4 h-4">
+								<Image
+									src="/images/spotify.png"
+									alt=""
+									width={48}
+									height={48}
+									className="w-4 h-4"
+								/>
+							</span>
+							Spotify
+						</>
+					)}
 				</p>
 			</div>
 		</div>
