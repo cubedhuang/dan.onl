0459de7f763a89070da855fb99030fedadc4fb52
Nigol <thetrollfacecompany@gmail.com>
2023-10-09 02:33:26 +0300

    Update skills.ts

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,93 +1,99 @@
 export interface SkillInfo {
-	name: string;
-	icon: string;
-	href: string;
-	bg?: string;
+  name: string;
+  icon: string;
+  href: string;
+  bg?: string;
 }
 
 export const skills: SkillInfo[] = [
-	{
-		name: "TypeScript",
-		icon: "ts",
-		href: "https://www.typescriptlang.org/",
-		bg: "#3279cc"
-	},
-	{
-		name: "JavaScript",
-		icon: "js",
-		href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-		bg: "#ecdb4f"
-	},
-	{
-		name: "HTML",
-		icon: "html",
-		href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-		bg: "#d55125"
-	},
-	{
-		name: "CSS",
-		icon: "css",
-		href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-		bg: "#3076bd"
-	},
-	{
-		name: "Rust",
-		icon: "rust",
-		href: "https://www.rust-lang.org/",
-		bg: "#d73c22"
-	},
-
-	{
-		name: "MongoDB",
-		icon: "mongodb",
-		href: "https://www.mongodb.com/",
-		bg: "#0f3430"
-	},
-	{
-		name: "Discord API",
-		icon: "bots",
-		href: "https://discord.com/developers/docs/",
-		bg: "#5663e9"
-	},
-	{
-		name: "Docker",
-		icon: "docker",
-		href: "https://www.docker.com/",
-		bg: "#4793eb"
-	},
-	{
-		name: "Node.js",
-		icon: "nodejs",
-		href: "https://nodejs.org/en/"
-	},
-	{
-		name: "GitHub",
-		icon: "github",
-		href: "https://github.com/"
-	},
-	{
-		name: "Python",
-		icon: "python",
-		href: "https://www.python.org/"
-	},
-	{
-		name: "Linux Systems",
-		icon: "linux",
-		href: "https://www.linux.org/"
-	},
-	{
-		name: "Tailwind CSS",
-		icon: "tailwind",
-		href: "https://tailwindcss.com/"
-	},
-	{
-		name: "Next.js",
-		icon: "nextjs",
-		href: "https://nextjs.org/"
-	},
-	{
-		name: "React",
-		icon: "react",
-		href: "https://reactjs.org/"
-	}
+  {
+    name: "JavaScript",
+    icon: "js",
+    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    bg: "#ecdb4f",
+  },
+  {
+    name: "HTML",
+    icon: "html",
+    href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    bg: "#d55125",
+  },
+  {
+    name: "CSS",
+    icon: "css",
+    href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+    bg: "#3076bd",
+  },
+  {
+    name: "Rust",
+    icon: "rust",
+    href: "https://www.rust-lang.org/",
+    bg: "#d73c22",
+  },
+  {
+    name: "Docker",
+    icon: "docker",
+    href: "https://www.docker.com/",
+    bg: "#4793eb",
+  },
+  {
+    name: "Node.js",
+    icon: "nodejs",
+    href: "https://nodejs.org/en/",
+  },
+  {
+    name: "GitHub",
+    icon: "github",
+    href: "https://github.com/",
+  },
+  {
+    name: "Python",
+    icon: "python",
+    href: "https://www.python.org/",
+  },
+  {
+    name: "Linux Systems",
+    icon: "linux",
+    href: "https://www.linux.org/",
+  },
+  {
+    name: "MySQL",
+    icon: "mysql",
+    href: "https://www.mysql.com/",
+  },
+  {
+    name: "PowerShell",
+    icon: "powershell",
+    href: "https://docs.microsoft.com/en-us/powershell/",
+  },
+  {
+    name: "Bash",
+    icon: "bash",
+    href: "https://www.gnu.org/software/bash/",
+  },
+  {
+    name: "FreeBSD",
+    icon: "freebsd",
+    href: "https://www.freebsd.org/",
+  },
+  {
+    name: "DNS Servers",
+    icon: "dns",
+    href: "https://en.wikipedia.org/wiki/Domain_Name_System",
+  },
+  {
+    name: "Elasticsearch / Elastic Stack",
+    icon: "elasticsearch",
+    href: "https://www.elastic.co/",
+  },
+  {
+    name: "Git",
+    icon: "git",
+    href: "https://git-scm.com/",
+  },
+  {
+    name: "GitLab",
+    icon: "gitlab",
+    href: "https://about.gitlab.com/",
+  },
 ];
