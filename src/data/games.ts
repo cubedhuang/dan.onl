a6d78205797cd6e90ae8e42a46432dff6816b429
Nigol <thetrollfacecompany@gmail.com>
2023-10-09 02:43:51 +0300

    Create games.ts

---
+++ src/data/games.ts
@@ -0,0 +1,16 @@
+export interface GameInfo {
+  name: string;
+  url: string;
+}
+
+export const games: GameInfo[] = [
+  {
+    name: "Game 1",
+    url: "https://example.com/game1",
+  },
+  {
+    name: "Game 2",
+    url: "https://example.com/game2",
+  },
+  // Add more games as needed
+];
