9d4c7c7ee60a0c6d8da1d021bea830d45ec11fb1
Nigol <thetrollfacecompany@gmail.com>
2023-10-09 01:33:18 +0300

    Update socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,27 +1,28 @@
 export interface SocialInfo {
-	name: string;
-	image: string;
-	url: string;
+    name: string;
+    image: string;
+    url: string;
 }
 
 export const socials: SocialInfo[] = [
-	{
-		name: "GitHub",
-		image: "/images/github.png",
-		url: "https://github.com/raitnigol"
-	},
-	{
-		name: "Facebook",
-		image: "/images/facebook.png",
-		url: "https://facebook.com/rait.nigol"
-	},
-	{
-		name: "LinkedIn",
-		image: "/images/linkedin.png",
-		url: "https://www.linkedin.com/in/raitnigol/"
-	{
-		name: "Spotify",
-		image: "/images/spotify.png",
-		url: "https://open.spotify.com/user/1190538422"
-	}
+    {
+        name: "GitHub",
+        image: "/images/github.png",
+        url: "https://github.com/raitnigol"
+    },
+    {
+        name: "Facebook",
+        image: "/images/facebook.png",
+        url: "https://facebook.com/rait.nigol"
+    },
+    {
+        name: "LinkedIn",
+        image: "/images/linkedin.png",
+        url: "https://www.linkedin.com/in/raitnigol/"
+    },
+    {
+        name: "Spotify",
+        image: "/images/spotify.png",
+        url: "https://open.spotify.com/user/1190538422"
+    }
 ];
